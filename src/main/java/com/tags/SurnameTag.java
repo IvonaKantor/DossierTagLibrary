6c39730edada878769dabf7b293e473b822d6422
ivona <ik87056@student.uwb.edu.pl>
2025-08-18 12:38:35 +0200

    update

--- src/main/java/com/tags/SurnameTag.java
+++ src/main/java/com/tags/SurnameTag.java
@@ -1,4 +1,4 @@
-package com.lab5.tags;
+package com.tags;
 
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
@@ -9,6 +9,6 @@ public class SurnameTag extends SimpleTagSupport {
     @Override
     public void doTag() throws JspException, IOException {
         JspWriter out = getJspContext().getOut();
-        out.print("<input type='text' name='surname' required>");
+        out.print("<input type='text' name='surname' placeholder='Surname' required>");
     }
 }
