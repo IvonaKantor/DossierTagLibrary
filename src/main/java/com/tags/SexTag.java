fe87630121358519eea33e34b4ea46d4d487d9e6
ivona <ik87056@student.uwb.edu.pl>
2025-08-22 23:42:58 +0200

    created a sex tag

---
+++ src/main/java/com/tags/SexTag.java
@@ -0,0 +1,10 @@
+package com.tags;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+
+public class SexTag extends SimpleTagSupport {
+
+}
