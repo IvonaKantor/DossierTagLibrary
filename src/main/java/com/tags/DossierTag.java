fee8ac47531b941e68f7da644d4e648f0bf0b237
ivona <ik87056@student.uwb.edu.pl>
2025-08-22 23:38:26 +0200

    created a dossier tag

---
+++ src/main/java/com/tags/DossierTag.java
@@ -0,0 +1,11 @@
+package com.tags;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+import javax.servlet.jsp.tagext.BodyContent;
+import java.io.IOException;
+
+public class DossierTag extends BodyTagSupport {
+
+}
