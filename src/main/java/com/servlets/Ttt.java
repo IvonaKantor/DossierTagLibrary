edd4a7302ec855a8c0d3df12f0cc9c2a2170aa5b
ivona <ik87056@student.uwb.edu.pl>
2025-08-24 23:40:58 +0200

    improved parameter processing

--- src/main/java/com/servlets/Ttt.java
+++ src/main/java/com/servlets/Ttt.java
@@ -1,15 +1,17 @@
 package com.servlets;
 
 import javax.servlet.*;
-import javax.servlet.http;
-import javax.servlet.annotation;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(name = "Ttt", urlPatterns = "/Ttt")
 public class Ttt extends HttpServlet {
+
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
@@ -22,9 +24,16 @@ public class Ttt extends HttpServlet {
             out.println("<h1>Data entered: </h1>");
             out.println("<p>Lastname: " + request.getParameter("lastname") + "</p>");
             out.println("<p>Surname: " + request.getParameter("surname") + "</p>");
-            out.println("<p>Sex: " +
-                    ("male".equals(request.getParameter("sex")) ? "Male" : "Female") + "</p>");
-            out.println("</div>");
+
+            String sex = request.getParameter("sex");
+            if ("male".equals(sex)) {
+                out.println("<p>Sex: Male</p>");
+            } else if ("female".equals(sex)) {
+                out.println("<p>Sex: Female</p>");
+            } else {
+                out.println("<p>Sex: Not specified</p>");
+            }
+
             out.println("<br>");
             out.println("<a href='Ttt.jsp'>Back to Form</a>");
             out.println("</body>");
@@ -33,10 +42,10 @@ public class Ttt extends HttpServlet {
             out.close();
         }
     }
-}
 
-@Override
-public void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-    response.sendRedirect("Ttt.jsp");
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendRedirect("Ttt.jsp");
+    }
 }
