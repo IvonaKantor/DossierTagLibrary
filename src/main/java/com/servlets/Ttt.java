1d60a3928c6b138dad9280897f143468f8cff473
ivona <ik87056@student.uwb.edu.pl>
2025-08-24 23:07:41 +0200

    added a redirect get requests to the form

--- src/main/java/com/servlets/Ttt.java
+++ src/main/java/com/servlets/Ttt.java
@@ -1,25 +1,20 @@
 package com.servlets;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http;
+import javax.servlet.annotation;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(name = "Ttt", urlPatterns = "/Ttt")
 public class Ttt extends HttpServlet {
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        if("Cancel".equals(request.getParametr("submit"))){
-            response.sendRedirect("index.html");
-            return;
-        }
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
 
-        try{
+        try {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head><title>Results</title></head>");
@@ -29,10 +24,19 @@ public class Ttt extends HttpServlet {
             out.println("<p>Surname: " + request.getParameter("surname") + "</p>");
             out.println("<p>Sex: " +
                     ("male".equals(request.getParameter("sex")) ? "Male" : "Female") + "</p>");
+            out.println("</div>");
+            out.println("<br>");
+            out.println("<a href='Ttt.jsp'>Back to Form</a>");
             out.println("</body>");
             out.println("</html>");
-        }finally{
+        } finally {
             out.close();
         }
     }
+}
+
+@Override
+public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+    response.sendRedirect("Ttt.jsp");
 }
